95341583a351857a6611833c930a40d6c06ea99b
alexgaudiosi <aa.gaudiosi@gmail.com>
2021-08-25 14:49:18 +0100

    ADD poro logo to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
-import { GatsbyImage } from 'gatsby-plugin-image';
 import posed from 'react-pose';
 import { Container } from './header.css';
 import Title from 'components/title';
@@ -12,11 +11,11 @@ const AnimatedContainer = posed.div({});
 const Header = ({ title }) => (
   <AnimatedContainer style={{ maxWidth: '1100px', margin: 'auto' }}>
     <Container>
-      <Link to="/">
-        <GatsbyImage
-          image={}
-          alt={title}
-          style={{ border: '1px solid' }}
+      <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+          src="./poro.svg"
+          alt=""
+          style={{ width: '40px', height: '40px', paddingRight: '10px' }}
         />
         <Title as="h1">{title}</Title>
       </Link>
