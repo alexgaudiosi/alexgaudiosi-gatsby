b060ff217e4e6d49bd3a80f3f604b48eddd2645a
alexgaudiosi <aa.gaudiosi@gmail.com>
2021-08-25 14:28:34 +0100

    ADD gatsbyimage to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
 import posed from 'react-pose';
 import { Container } from './header.css';
 import Title from 'components/title';
@@ -12,6 +13,11 @@ const Header = ({ title }) => (
   <AnimatedContainer style={{ maxWidth: '1100px', margin: 'auto' }}>
     <Container>
       <Link to="/">
+        <GatsbyImage
+          image={}
+          alt={title}
+          style={{ border: '1px solid' }}
+        />
         <Title as="h1">{title}</Title>
       </Link>
       <Nav />
