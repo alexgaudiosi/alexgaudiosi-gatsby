a841130b8829fa10f4d85149ee78ac3598ba869d
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2026-09-22 10:38:42 +0100

    ADD new logos

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
     },
     'gatsby-plugin-sitemap',
     'gatsby-plugin-offline',
+    'gatsby-plugin-styled-components',
     'gatsby-plugin-netlify',
     'gatsby-transformer-json',
     {
